f92919711cd928089c8c079542f438c23b22bda7
Damien Gaignon <damien.gaignon@gmail.com>
2021-03-13 15:03:54 +0100

    Change setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 readme = open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r').read()
 
 setup(
-    name='virtualenv-tools3',
-    author='Fireteam Ltd.; Yelp, Inc.',
-    author_email='opensource@yelp.com',
-    version='2.0.4',
-    url='http://github.com/Yelp/virtualenv-tools',
+    name='virtualenv-tools-enhanced',
+    author='Fireteam Ltd.; Yelp, Inc.; Damien "psolyca" Gaignon',
+    author_email='damien.gaignon@gmail.com',
+    version='1.0.0',
+    url='https://github.com/psolyca/virtualenv-tools',
     py_modules=['virtualenv_tools'],
-    description='A set of tools for virtualenv',
+    description='A set of tools to relocate virtualenv (and main installation on Windows)',
     long_description=readme,
     long_description_content_type='text/markdown',
     entry_points={
@@ -22,12 +22,10 @@ setup(
     zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ]
 )
